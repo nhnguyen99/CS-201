4b546874cfd75b016381cb8114d3f88d7e41eb68
Nguyen Hung Nguyen <38390358+nhnguyen99@users.noreply.github.com>
2020-08-11 01:39:45 -0500

    Add files via upload

--- phase1main.cpp
+++ phase1main.cpp
@@ -49,7 +49,7 @@ int main(){
 	cout << "Select is " << A.Select(3) << endl;
 	// A => "10 0 1 2 3 4 5 6 7 8 9" Select => 2
 	cout << "Search is " << A.Search(5) << endl;
-	// A => "10 0 1 2 3 4 5 6 7 8 9" Search => 5
+	// A => "10 0 1 2 3 4 5 6 7 8 9" Search => 6
 	A.InsertionSort();
 	// A => "0 1 2 3 4 5 6 7 8 9 10"
 	cout << "SetOrdered is " << A.SetOrdered() << endl;
